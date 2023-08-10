4a3da46f72c12818470d5e2b86439afe81797599
JesúsMendoza <mendoza.git@gmail.com>
2023-08-09 18:39:47 -0600

    Invoice Validation Models added

---
+++ Pacs/Sw/InvoiceRetrieving/Input/InvoiceRetrieveInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Sosoem.FiscalAPI.Dtos.Pacs.Sw.InvoiceRetrieving.Input;
+
+public class InvoiceRetrieveInputModel
+{
+    public string InvoiceUuid { get; set; } = string.Empty;
+
+    public string IssuerTin { get; set; } = string.Empty;
+
+    public string RecipientTin { get; set; } = string.Empty;
+    public string InvoiceAmount { get; set; } = string.Empty;
+
+    /// <summary>
+    ///  Last eight digits of the Signature Value
+    /// </summary>
+    public string InvoiceSignatureValue { get; set; } = string.Empty;
+}
---
+++ Pacs/Sw/InvoiceValidation/Output/InvoiceValidationOutputModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Sosoem.FiscalAPI.Dtos.Pacs.Sw.InvoiceValidation.Output;
+
+public class InvoiceValidationOutputModel
+{
+    [JsonPropertyName("status")] 
+    public string? Status { get; set; }
+
+
+    [JsonPropertyName("cadenaOriginalSAT")]
+    public string? TaOriginalString { get; set; }
+
+    [JsonPropertyName("cadenaOriginalComprobante")]
+    public string? InvoiceOriginalString { get; set; }
+
+    [JsonPropertyName("uuid")] public string? Uuid { get; set; }
+
+    [JsonPropertyName("statusSat")] public string? StatusSat { get; set; }
+
+    [JsonPropertyName("statusCodeSat")] public string? StatusCodeSat { get; set; }
+
+
+    [JsonPropertyName("isCancelable")] public string? IsCancelable { get; set; }
+
+    [JsonPropertyName("statusCancelation")]
+    public string? CancelationStatus { get; set; }
+
+
+    [JsonPropertyName("detail")] public List<Error> Errors { get; set; } = new();
+}
+
+public class Error
+{
+    [JsonPropertyName("detail")] public List<ErrorDetail> Errors { get; set; } = new();
+    [JsonPropertyName("section")] public string? ErrorSection { get; set; }
+}
+
+public class ErrorDetail
+{
+    [JsonPropertyName("message")] public string? Message { get; set; }
+    [JsonPropertyName("messageDetail")] public string? MessageDetail { get; set; }
+    [JsonPropertyName("type")] public int Type { get; set; }
+    [JsonPropertyName("typeValue")] public string? TypeValue { get; set; }
+}
