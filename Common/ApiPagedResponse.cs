bf385f1b9e0a5681f7744e4e05f2ae7719fe02fa
JesúsMendoza <mendoza.git@gmail.com>
2023-12-15 07:54:11 -0600

    Ignore quey filters added

--- Common/ApiPagedResponse.cs
+++ Common/ApiPagedResponse.cs
@@ -4,26 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sosoem.FiscalAPI.Dtos.Common
-{
-    public class ApiPagedResponse<T> : ApiResponse<T>
-    {
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
-        public Uri? FirstPage { get; set; }
-        public Uri? LastPage { get; set; }
-        public int TotalPages { get; set; }
-        public int TotalRecords { get; set; }
-        public Uri? NextPage { get; set; }
-        public Uri? PreviousPage { get; set; }
+namespace Sosoem.FiscalAPI.Dtos.Common;
 
-        public ApiPagedResponse(T data, int pageNumber, int pageSize)
-        {
-            PageNumber = pageNumber;
-            PageSize = pageSize;
-            Data = data;
-            Message = null;
-            Succeeded = true;
-        }
+public class ApiPagedResponse<T> : ApiResponse<T>
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public Uri? FirstPage { get; set; }
+    public Uri? LastPage { get; set; }
+    public int TotalPages { get; set; }
+    public int TotalRecords { get; set; }
+    public Uri? NextPage { get; set; }
+    public Uri? PreviousPage { get; set; }
+
+    public ApiPagedResponse(T data, int pageNumber, int pageSize)
+    {
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        Data = data;
+        Message = null;
+        Succeeded = true;
     }
 }
